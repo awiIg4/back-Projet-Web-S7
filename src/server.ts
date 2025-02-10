95cb04a32ecd8dea1d0e76a5b896af06bd43e038
hugobrun <hugo.brun@etu.umontpellier.fr>
2025-02-10 15:19:07 +0100

    Fix: updated licence import logic using findOne and create

--- src/server.ts
+++ src/server.ts
@@ -77,7 +77,7 @@ async function setupDatabase() {
     console.log('✅ Database sync complete.');
 
     console.log('🌱 Running database seeder...');
-    console.log('JAIME LE JAMBON BEAUCOUP BEAUCOUP BEAUCOUP');
+    console.log('JAIME LE JAMBON BEAUCOUP BEAUCOUP BEAUCOUP CA FAIT CHIER DE FOUUUUUU');
     await runSeeder();
     console.log('✅ Database seeding completed.');
   } catch (error) {
